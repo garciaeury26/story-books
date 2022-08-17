52a1703b6220482dc4858631cec78789619ff81e
eury joel <garciaeury26@gmail.com>
2022-08-16 21:48:43 -0400

    build de storybook removido del gitinore

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,18 +1,46 @@
 import "./MyLabel.css";
-import React from "react";
 
+// puedo documentar la props para storybooks con conmentarios
 export interface MyLabelProps {
+    /**
+    * Convert the label in uppercase
+    */
+    allInCapital: boolean,
+    /** 
+     * Color of the label
+   */
+    color?: "primary" | "secondary" | "tertiary",
+    /**
+     * Title of the text
+     */
     label: string,
-    size?: number | string,
-    style?: "bold" | "normal"
+    /**
+    * Size of the text
+    */
+    size?: "h1" | "h2" | "h3",
+    /**
+     * Style of the text
+     */
+    style?: "bold" | "italic",
+    /**
+    * Custom font color
+    */
+    fontColor?: string
 }
 
-export const MyLabel = ({ label = "Label", size = "normal", style = "bold" }: MyLabelProps) => {
+export const MyLabel = ({
+    allInCapital = false,
+    color,
+    label = "Label",
+    size = "h3",
+    style = "italic",
+    fontColor
+
+}: MyLabelProps) => {
     return (
-        <span style={{
-            fontWeight: style,
-            fontSize: `${size}px` || "16px",
-        }}>
+        <span
+            style={{ color: fontColor }}
+            className={`label ${size} ${style} text-${color} ${allInCapital && "capitalize"}`}>
             {label}
         </span>
     )
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -1,23 +1,59 @@
 // Crear una historia
 
-import { MyLabel, MyLabelProps } from "../../components/MyLabel"
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { MyLabel } from "../../components/MyLabel"
 
 // 1- exportqcion por defecto
 export default {
     // con el title le indico en que grupo se va a grupar
     // tambien le puedo dar un nombre al componente con => Optional:/nombre
     title: "UI/Titles/MyLabel",
-    component: MyLabel
-}
+    component: MyLabel,
+    //control props => personalizado
+    argTypes: {
+        fontColor: {
+            control: {
+                type: "color",
+                presetColors: ['#ff0000', '#00ff00', '#0000ff'],
+            }
+        }
+    }
+} as ComponentMeta<typeof MyLabel>;
 
 // definicion del template o base
-const Template = (args: MyLabelProps) => <MyLabel {...args} />
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args} />
 
 // modelo variacion del template 
-export const Basic = Template.bind({})
+export const Basic = Template.bind({});
+Basic.args = {
+    label: 'Basic',
+}
 
 // medium
 export const Medium = Template.bind({})
+Medium.args = {
+    label: 'Medium',
+    size: "h2",
+    style: "bold",
+    color: "tertiary",
+}
 
 // large
-export const Large = Template.bind({})
+export const Large = Template.bind({});
+Large.args = {
+    label: 'Large',
+    size: "h1",
+    style: "italic",
+    allInCapital: true,
+}
+
+// color personalizado
+export const CustomColor = Template.bind({});
+CustomColor.args = {
+    label: 'Custom Color',
+    size: "h1",
+    style: "bold",
+    allInCapital: true,
+    fontColor: "#59d669"
+}
